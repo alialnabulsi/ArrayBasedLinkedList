bf121a5c6dfc78873be255d82c80579e9b20371d
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-04-29 17:07:34 +0300

    Organized

--- include/List.h
+++ include/List.h
@@ -1,87 +1,128 @@
-// List.h
-
-#include <iostream>
 #ifndef LIST_H
 #define LIST_H
-#include "NodePool.h"
 
+#include <iostream>
+#include <functional>
+#include "NodePool.h"
 using namespace std;
 
-class List{
-    public:
-        List();
-        List(const List& other);
-        List& operator=(const List& other);
-        ~List();
-        bool isEmpty() const;
-        void traverse(const function<void(const T&)>& visit) const;
-        int size() const;
-        int search(const T& item) const;
-        void insertAtEnd(const T& item);
-        void deleteAtEnd();
-        void insertAtTop(const T& item);
-        void deleteAtTop();
-        void insertAfterPos(int pos, const T& item);
-        bool delete(const T& item);
-        int getFree() const;
-        void sortList();
-        void isSorted();
-        void display() const;
-    private:
-        NodePool<T> pool;  // Node pool for memory management
-        int first;  // First element in the list
+template<typename T>
+class List {
+public:
+    List();
+    List(const List& other);
+    List& operator=(const List& other);
+    ~List();
+    bool isEmpty() const;
+    void traverse(const function<void(const T&)>& visit) const;
+    int size() const;
+    int search(const T& item) const;
+    void insertAtEnd(const T& item);
+    void deleteAtEnd();
+    void insertAtTop(const T& item);
+    void deleteAtTop();
+    void insertAfterPos(int pos, const T& item);
+    bool deleteItem(const T& item);
+    int getFree() const;
+    void sortList();
+    bool isSorted() const;
+    void display() const;
+
+private:
+    NodePool<T> pool;
+    int first;
 };
 
-template<typename ElementType>
-List<ElementType>::List(): pool(), first(NULL_VALUE){
+template<typename T>
+List<T>::List() : pool(), first(NULL_VALUE) {}
 
+template<typename T>
+List<T>::List(const List& other) : pool(), first(NULL_VALUE) {
+    int otherPtr = other.first;
+    int lastNewNode = NULL_VALUE;
+
+    while (otherPtr != NULL_VALUE) {
+        int newNodeIndex = pool.newNode();
+        pool.getPool()[newNodeIndex].data = other.pool.getPool()[otherPtr].data;
+
+        if (lastNewNode == NULL_VALUE) {
+            first = newNodeIndex;
+        } else {
+            pool.getPool()[lastNewNode].next = newNodeIndex;
+        }
+
+        lastNewNode = newNodeIndex;
+        otherPtr = other.pool.getPool()[otherPtr].next;
+    }
 }
-template<typename ElementType>
-List<ElementType>::List(const List& other){
 
+template<typename T>
+List<T>& List<T>::operator=(const List& other) {
+    if (this != &other) {
+        while (!isEmpty()) {
+            deleteAtTop();
+        }
+
+        int otherPtr = other.first;
+        int lastNewNode = NULL_VALUE;
+
+        while (otherPtr != NULL_VALUE) {
+            int newNodeIndex = pool.newNode();
+            pool.getPool()[newNodeIndex].data = other.pool.getPool()[otherPtr].data;
+
+            if (lastNewNode == NULL_VALUE) {
+                first = newNodeIndex;
+            } else {
+                pool.getPool()[lastNewNode].next = newNodeIndex;
+            }
+
+            lastNewNode = newNodeIndex;
+            otherPtr = other.pool.getPool()[otherPtr].next;
+        }
+    }
+    return *this;
 }
 
-template<typename ElementType>
-List& List<ElementType> operator=(const List& other){
-
+template<typename T>
+List<T>::~List() {
+    while (!isEmpty()) {
+        deleteAtTop();
+    }
 }
 
-template<typename ElementType>
-List<ElementType>::~List(){
-
+template<typename T>
+bool List<T>::isEmpty() const {
+    return first == NULL_VALUE;
 }
 
-template<typename ElementType>
-void List<ElementType>::traverse(const function<void(const T&)>& visit) const;
-
-template<typename ElementType>
-int List<ElementType>::size() const{
-    int count = 0 , ptr = first;
-    while(size != NULL_VALUE){
+template<typename T>
+int List<T>::size() const {
+    int count = 0, ptr = first;
+    while(ptr != NULL_VALUE) {
         count++;
         ptr = pool.getPool()[ptr].next;
-
     }
     return count;
 }
-template<typename ElementType>
-int List<ElementType>::search(const T& item) const{
-    int pos = NULL_VALUE , ptr = first;
-    while(ptr != NULL_VALUE){
-        if (pool.getPool()[ptr].data == item){
+
+template<typename T>
+int List<T>::search(const T& item) const {
+    int pos = NULL_VALUE, ptr = first;
+    while(ptr != NULL_VALUE) {
+        if (pool.getPool()[ptr].data == item) {
             pos = ptr;
         }
-
         ptr = pool.getPool()[ptr].next;
     }
     return pos;
 }
-template<typename ElementType>
-void List<ElementType>::insertAtEnd(const T& item){
+
+template<typename T>
+void List<T>::insertAtEnd(const T& item) {
     int ptr = first;
     int newNodeIndex;
-    while(ptr != NULL_VALUE){
-        if(pool.getPool()[ptr].next == NULL_VALUE){
+    while(ptr != NULL_VALUE) {
+        if(pool.getPool()[ptr].next == NULL_VALUE) {
             newNodeIndex = pool.newNode();
             pool.getPool()[newNodeIndex].data = item;
         }
@@ -89,41 +130,41 @@ void List<ElementType>::insertAtEnd(const T& item){
     }
 }
 
-template<typename ElementType>
-void List<ElementType>::deleteAtEnd(){
+template<typename T>
+void List<T>::deleteAtEnd() {
     int ptr = first;
-    while(ptr != NULL_VALUE){
-        if(pool.getPool()[ptr].next == NULL_VALUE){
+    while(ptr != NULL_VALUE) {
+        if(pool.getPool()[ptr].next == NULL_VALUE) {
             pool.deleteNode(ptr);
         }
         ptr = pool.getPool()[ptr].next;
     }
 }
 
-template<typename ElementType>
-void List<ElementType>::insertAtTop(const T& item){
+template<typename T>
+void List<T>::insertAtTop(const T& item) {
     int index = pool.newNode();
     pool.getPool()[index].data = item;
     pool.getPool()[index].next = first;
     first = index;
 }
 
-template<typename ElementType>
-void List<ElementType>::deleteAtTop(){
+template<typename T>
+void List<T>::deleteAtTop() {
     if (isEmpty())
-        cerr << ("List::deleteFront() on empty list");
+        cerr << "List::deleteFront() on empty list";
     int index = first;
     first = pool.getPool()[index].next;
     pool.deleteNode(index);
 }
 
-template<typename ElementType>
-void List<ElementType>::insertAfterPos(int pos, const T& item){
+template<typename T>
+void List<T>::insertAfterPos(int pos, const T& item) {
     if (isEmpty()) {
-        cerr << ("List::insertAfter() on empty list ");
+        cerr << "List::insertAfter() on empty list";
     }
     if (pos == NULL_VALUE || pos < 0 || pos >= NUM_NODES)
-        cerr << ("List::insertAfter invalid position");
+        cerr << "List::insertAfter invalid position";
     int index = pool.newNode();
     pool.getPool()[index].data = item;
     int nextIndex = pool.getPool()[pos].next;
@@ -131,20 +172,95 @@ void List<ElementType>::insertAfterPos(int pos, const T& item){
     pool.getPool()[index].next = nextIndex;
 }
 
-template<typename ElementType>
-bool List<ElementType>::delete(const T& item){
-    int pos, ptr = first;
-    while(ptr != NULL_VALUE){
-        if(pool.getPool()[ptr].data == item){
+template<typename T>
+bool List<T>::deleteItem(const T& item) {
+    int prev = NULL_VALUE;
+    int curr = first;
 
+    while (curr != NULL_VALUE) {
+        if (pool.getPool()[curr].data == item) {
+            if (prev == NULL_VALUE) {
+                first = pool.getPool()[curr].next;
+            } else {
+                pool.getPool()[prev].next = pool.getPool()[curr].next;
+            }
+            pool.deleteNode(curr);
+            return true;
         }
-        ptr = pool.getPool()[ptr].next;
+        prev = curr;
+        curr = pool.getPool()[curr].next;
     }
     return false;
 }
 
-template<typename ElementType>
-int List<ElementType>::getFree() const{
+template<typename T>
+void List<T>::traverse(const function<void(const T&)>& visit) const {
+    int ptr = first;
+    while (ptr != NULL_VALUE) {
+        visit(pool.getPool()[ptr].data);
+        ptr = pool.getPool()[ptr].next;
+    }
+}
+
+template<typename T>
+void List<T>::sortList() {
+    bool swapped;
+    do {
+        swapped = false;
+        int prev = NULL_VALUE;
+        int curr = first;
+        int next = (curr != NULL_VALUE) ? pool.getPool()[curr].next : NULL_VALUE;
+
+        while (next != NULL_VALUE) {
+            if (pool.getPool()[curr].data > pool.getPool()[next].data) {
+                T temp = pool.getPool()[curr].data;
+                pool.getPool()[curr].data = pool.getPool()[next].data;
+                pool.getPool()[next].data = temp;
+                swapped = true;
+            }
+            prev = curr;
+            curr = next;
+            next = pool.getPool()[next].next;
+        }
+    } while (swapped);
+}
+
+template<typename T>
+bool List<T>::isSorted() const {
+    if (isEmpty() || pool.getPool()[first].next == NULL_VALUE) {
+        return true;
+    }
+
+    int curr = first;
+    int next = pool.getPool()[curr].next;
+
+    while (next != NULL_VALUE) {
+        if (pool.getPool()[curr].data > pool.getPool()[next].data) {
+            return false;
+        }
+        curr = next;
+        next = pool.getPool()[next].next;
+    }
+    return true;
+}
+
+template<typename T>
+void List<T>::display() const {
+    int ptr = first;
+    cout << "List: [";
+    while (ptr != NULL_VALUE) {
+        cout << pool.getPool()[ptr].data;
+        ptr = pool.getPool()[ptr].next;
+        if (ptr != NULL_VALUE) {
+            cout << ", ";
+        }
+    }
+    cout << "]" << endl;
+}
+
+template<typename T>
+int List<T>::getFree() const {
     return pool.getFree();
 }
 
+#endif
--- include/NodePool.h
+++ include/NodePool.h
@@ -1,130 +1,74 @@
-// NodePool.h
-
-/**
- * @file NodePool.h
- * @brief Implementation of a node pool for array-based linked list management
- *
- * This file contains the NodeType structure and NodePool class which together
- * provide a pool of nodes stored in an array. The pool manages allocation and
- * deallocation of nodes without dynamic memory operations.
- *
- * The NodePool maintains:
- * - An array of NodeType structs (storage pool)
- * - A free list to track available nodes
- * - Methods to allocate (newNode) and release (deleteNode) nodes
- */
-
-#include <iostream>
 #ifndef NODEPOOL_H
 #define NODEPOOL_H
 
+#include <iostream>
 using namespace std;
 
-const int NULL_VALUE = -1;  // Represents "no node"
-const int NUM_NODES = 2048; // Size of the storage pool
+const int NULL_VALUE = -1;
+const int NUM_NODES = 2048;
 
 template<typename T>
 struct NodeType {
     T data;
-    int next; // index of the next node, or NULL_VALUE
+    int next;
 };
 
 template<typename T>
 class NodePool {
 public:
-    /**
-     * @brief Constructs a NodePool with initialized free list
-     */
     NodePool();
-
-    /**
-     * @brief Allocates a new node from the free list
-     * @return Index of the allocated node
-     * @pre There should be available nodes in the pool
-     * @post The node is removed from free list and ready for use
-     */
     int newNode();
-
-    /**
-     * @brief Returns a node to the free list
-     * @param index Index of the node to deallocate
-     * @pre index must be valid (0 <= index < NUM_NODES)
-     * @post Node is added to the free list for reuse
-     */
     void deleteNode(int index);
-
-    /**
-     * @brief Gets the node pool array
-     * @return Pointer to the node array
-     */
     NodeType<T>* getPool() const;
-
-    /**
-     * @brief Gets the current free list head
-     * @return Index of the first free node
-     */
     int getFree() const;
 
 private:
     NodeType<T> arrNode[NUM_NODES];
     int free;
-
-    /**
-     * @brief Initializes the free list by linking all nodes
-     * @post All nodes are linked in sequence, free points to first node
-     */
     void initializePool();
 };
 
+template<typename T>
+NodePool<T>::NodePool() {
+    initializePool();
+}
 
+template<typename T>
+void NodePool<T>::initializePool() {
+    for(int i = 0; i < NUM_NODES - 1; i++) {
+        arrNode[i].next = i + 1;
+    }
+    arrNode[NUM_NODES - 1].next = NULL_VALUE;
+    free = 0;
+}
 
-template<typename ElementType>
-NodePool<ElementType>::NodePool(){
-    intializePool();
-  }
-
-template<typename ElementType>
-void NodePool<ElementType>::intializePool(){
-  for(int i = 0; i < NUM_NODES - 1; i++){
-    arrNode[i].next=i+1;
-    }S
-  arrNode[NUM_NODES - 1] = NULL_VALUE;
-  free = 0;
-  }
-
-template<typename ElementType>
-int NodePool<ElementType>::newNode(){
-   if(free == NULL_VALUE){
-     cerr << "No availiable space in the list";
-     }
-   int index = free;
-   free = arrNode[free].next;
-   arrNode[index].next = NULL_VALUE;
-
+template<typename T>
+int NodePool<T>::newNode() {
+    if(free == NULL_VALUE) {
+        cerr << "No available space in the list";
+    }
+    int index = free;
+    free = arrNode[free].next;
+    arrNode[index].next = NULL_VALUE;
     return index;
-  }
+}
 
-/*template<typename ElementType>
-void NodePool<ElementType>::deleteNode(){
-
-}*/
-
-template<typename ElementType>
-void NodePool<ElementType>::deleteNode(int index) {
+template<typename T>
+void NodePool<T>::deleteNode(int index) {
     if (index < 0 || index >= NUM_NODES)
-        cerr << ("NodePool: deleteNode index out of range");
-    pool[index].next = free;
+        cerr << "NodePool: deleteNode index out of range";
+    arrNode[index].next = free;
     free = index;
 }
-template<typename ElementType>
-NodeType* NodePool<ElementType>::getPool() const{
-    return pool;
+
+template<typename T>
+NodeType<T>* NodePool<T>::getPool() const {
+    return arrNode;
 }
 
-template<typename ElementType>
-int NodePool<ElementType>::getFree() const{
+template<typename T>
+int NodePool<T>::getFree() const {
     return free;
 }
-
 
 #endif
